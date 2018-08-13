e4d1fd69890c4f6a8a3a266ef7e903a525bb876d
nthony <nthony.kim@gmail.com>
2018-08-13 19:53:55 +0900

    jsp 수정 및 업데이트

--- src/com/dao/GoodsDAO.java
+++ src/com/dao/GoodsDAO.java
@@ -7,11 +7,8 @@ import org.apache.ibatis.session.SqlSession;
 import com.dto.GoodsDTO;
 
 public class GoodsDAO {
-	public List<GoodsDTO> goodsList(SqlSession session,
-			String goods_Category){
-		
-		List<GoodsDTO> list =
-	session.selectList("com.goods.goodsList", goods_Category);
+	public List<GoodsDTO> goodsList(SqlSession session, String goods_Category){
+		List<GoodsDTO> list = session.selectList("com.goods.goodsList", goods_Category);
 		return list;
 	}
 	
