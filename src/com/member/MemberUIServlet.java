eb6e34ee1b62ca3dfcfba85c4918c7c8f155439c
lionho <audar8@gmail.com>
2018-08-15 02:04:22 +0900

    Update MemberUIServlet.java
    수정

--- src/com/member/MemberUIServlet.java
+++ src/com/member/MemberUIServlet.java
@@ -7,23 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class MemberUIServlet
- */
 @WebServlet("/MemberUIServlet")
 public class MemberUIServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("loginForm.jsp");
+		response.sendRedirect("memberForm.jsp");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
