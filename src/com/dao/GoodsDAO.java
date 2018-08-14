fff26386c29d8d7a997e186927e68f52deddbbdd
Your Name <fontina10@naver.com>
2018-08-14 14:59:18 +0900

    test_wonsol

--- src/com/dao/GoodsDAO.java
+++ src/com/dao/GoodsDAO.java
@@ -10,6 +10,7 @@ public class GoodsDAO {
 	public List<GoodsDTO> goodsList(SqlSession session, String goods_Category){
 		List<GoodsDTO> list = session.selectList("com.goods.goodsList", goods_Category);
 		return list;
+		//price 오류 해결중 
 	}
 	
 	
