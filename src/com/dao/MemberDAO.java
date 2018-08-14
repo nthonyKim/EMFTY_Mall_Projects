a6c8e47dd45368a239b43551d86e5f4facb9f263
lionho <audar8@gmail.com>
2018-08-15 01:59:54 +0900

    Update MemberDAO.java
    아이디체크 추가

--- src/com/dao/MemberDAO.java
+++ src/com/dao/MemberDAO.java
@@ -1,14 +1,18 @@
-package com.dao;
+
 
 import org.apache.ibatis.session.SqlSession;
 
 import com.dto.MemberDTO;
 
 public class MemberDAO {
+	public int idCheck(SqlSession session, String userid) {
+		int n = session.selectOne("com.dto.idCheck", userid);
+		return n;
+	}
+	
 	public int memberAdd(SqlSession session, MemberDTO dto) {
 		int count = session.insert("com.dto.memberAdd", dto);
 		return count;
-		
 	}
 	
 }
