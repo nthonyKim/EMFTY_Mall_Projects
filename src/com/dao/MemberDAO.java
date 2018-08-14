68f4a70dd724a9043a343d9aaf34cbef01d4264c
nthonyKim <42129313+nthonyKim@users.noreply.github.com>
2018-08-14 18:39:43 +0900

    Update MemberDAO.java
    a

--- src/com/dao/MemberDAO.java
+++ src/com/dao/MemberDAO.java
@@ -1,5 +1,14 @@
 package com.dao;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.dto.MemberDTO;
+
 public class MemberDAO {
+	public int memberAdd(SqlSession session, MemberDTO dto) {
+		int count = session.insert("com.dto.memberAdd", dto);
+		return count;
 		
+	}
+	
 }
