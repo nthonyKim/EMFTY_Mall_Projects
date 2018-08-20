273c4197bad35c07651e348879dad67f3d574e07
Your Name <fontina10@naver.com>
2018-08-20 16:02:17 +0900

    0820_wonsol_2

--- src/com/dao/BoardDAO.java
+++ src/com/dao/BoardDAO.java
@@ -1,5 +1,16 @@
 package com.dao;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.dto.BoardDTO;
+
 public class BoardDAO {
 
+	
+			public BoardDTO selectNotice(SqlSession session, 
+		BoardDTO dto) {
+				return session.selectOne("com.board.selectNotice", dto); 
+			
+			
 }
+}//end dao
