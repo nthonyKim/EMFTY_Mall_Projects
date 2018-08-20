60b4521977a3583c83f806b141ead3b7e6321755
Your Name <fontina10@naver.com>
2018-08-20 16:06:20 +0900

    0820_wonsol_3

--- src/com/board/selectNotice.java
+++ src/com/board/selectNotice.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.dto.BoardDTO;
 import com.dto.GoodsDTO;
-import com.service.BoardService;
+/*import com.service.BoardService;*/
 
 /**
  * Servlet implementation class GoodsListServlet
@@ -20,7 +20,7 @@ import com.service.BoardService;
 @WebServlet("/BoardListServlet")
 public class selectNotice extends HttpServlet {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	/*protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 		String strSeq = request.getParameter("seq")==null ? "1" : request.getParameter("seq");
 		
@@ -29,18 +29,18 @@ public class selectNotice extends HttpServlet {
 		param.setSeq(Integer.parseInt(strSeq));
 		List<BoardDTO> boardList = service.selectBoardList(param);
 		
-		request.setAttribute("boardList", boardList);
+		request.setAttribute("boardList", boardList); //master 병합용
 		
 		RequestDispatcher dis = request.getRequestDispatcher("boardList.jsp");
 		dis.forward(request, response);
 	}
 
-	/**
+	*//**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	 *//*
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-	}
+	}*/
 
 }
