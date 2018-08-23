d0673bde4eca69dfad5cf0db3e84aa6caf297a21
Your Name <fontina10@naver.com>
2018-08-23 18:26:04 +0900

    게시판상세보기 작업중_wonsol_3

---
+++ src/com/board/QuestionBoardDetailServlet.java
@@ -0,0 +1,51 @@
+package com.board;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import com.dto.QuestionBoardDTO;
+
+import com.service.QuestionBoardService;
+
+/**
+ * Servlet implementation class MyBoardListServlet
+ */
+@WebServlet("/QuestionBoardDetailServlet")
+public class QuestionBoardDetailServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("questionCheck");
+		request.setCharacterEncoding("UTF-8");
+	
+		String searchName= request.getParameter("searchName");
+		String searchValue= request.getParameter("searchValue");
+		
+		HashMap<String, String> map = new HashMap<>();
+		map.put("searchName", searchName);
+		map.put("searchValue", searchValue);
+		
+		QuestionBoardService service = new  QuestionBoardService();
+		List<QuestionBoardDTO> list = service.select(map);
+		
+		request.setAttribute("list", list);
+		System.out.println(list);
+		RequestDispatcher dis = 
+				request.getRequestDispatcher("QuestionBoardDetail.jsp");
+		dis.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
